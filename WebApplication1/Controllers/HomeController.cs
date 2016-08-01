fc3a5ed0f8896f7788c702ed812190cc12c3fbec
Steve O'Gallagher <Steve O'Gallagher>
2016-08-01 17:05:49 +0100

    Display all known usernames in Bowling page.

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,14 +1,31 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     [RequireHttps]
     public class HomeController : Controller
     {
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -24,11 +41,24 @@ namespace WebApplication1.Controllers
             return new EmptyResult();
         }
 
-        public ActionResult About()
+        public ActionResult Bowling()
         {
-            ViewBag.Message = "Your application description page.";
+            var users = new List<string>();
+            var knownUsers = UserManager.Users.ToList();
+            
+            foreach( var u in knownUsers)
+            {
+                users.Add(u.UserName);
+            };
 
-            return View();
+            var model = new BowlingViewModel
+            {
+                UserNames = users
+            };
+
+            
+
+            return View(model);
         }
 
         public ActionResult Contact()
---
+++ WebApplication1/Models/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace WebApplication1.Models
+{
+    public class BowlingViewModel
+    {
+        public List<string> UserNames { get; set; }
+    }
+}
